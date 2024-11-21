1aee929a28d2588f80b3293f23d18a6c94e8b6da
WendelF <wendelfas@hotmail.com>
2024-11-21 12:51:45 -0300

    CRUD testing

--- src/components/menu/BtnsOptions.jsx
+++ src/components/menu/BtnsOptions.jsx
@@ -1,26 +1,18 @@
 import App from '/src/App.jsx'
 import Options from './Options';
 
-const BtnsOptions = (removerBtn, criarBtn) => {
 
-    function removerBtn() {
-        let botaoexcluido = document.querySelectorAll('.botaozin')
-        if (botaoexcluido.length > 0) {
-            botaoexcluido[0].remove()
-        } else {
-            console.log('não tem botão cadastrado')
-            console.log(botaoexcluido)
-        }
-    }
+const BtnsOptions = ({ idBtn, contentBtn, typeBtn }) => {
 
-    function criarBtn() {
-        const botaoteste = document.createElement("button")
-        botaoteste.innerText = "teste"
-        botaoteste.className = "botaozin"
-        menudiv.appendChild(botaoteste)
-    }
+
+    return <>
+        <button id={idBtn} onClick={typeBtn}>
+            {contentBtn}
+        </button>
+    </>
 
 
 }
+
 
 export default BtnsOptions
--- src/components/menu/Options.jsx
+++ src/components/menu/Options.jsx
@@ -1,21 +1,37 @@
 import { useState } from "react";
 import BtnsOptions from "./BtnsOptions.jsx";
 
+function criarBtn() {
+    const botaoteste = document.createElement("button")
+    botaoteste.innerText = "teste"
+    botaoteste.className = "botaozin"
+    menudiv.appendChild(botaoteste)
+}
+
+function removerBtn() {
+    let botaoexcluido = document.querySelectorAll('.botaozin')
+    if (botaoexcluido.length > 0) {
+        botaoexcluido[0].remove()
+    } else {
+        console.log('não tem botão cadastrado')
+        console.log(botaoexcluido)
+    }
+}
+
 const Options = () => {
     //const [texto, setTexto] = useState("Testando") - É assim que se declara um useState. OBS: Importante lembrar de importar.
     return (
         <>
-            <button id={'criar'} onClick={BtnsOptions}>
-                Criar Botao
-            </button>
-            <button id={'remover'} onClick={BtnsOptions}>
-                Remover Botão
-            </button>
-            <button id={'editar'} onClick={BtnsOptions}>
-                Remover Botão
-            </button>
-
-            
+            <BtnsOptions 
+            idBtn={'criar'} 
+            contentBtn={'Criar um botão de teste'}
+            typeBtn={criarBtn}
+            />
+            <BtnsOptions 
+            idBtn={'remover'} 
+            contentBtn={'Remover um botão de teste'}
+            typeBtn={removerBtn}
+            />
         </>
     );
 }
